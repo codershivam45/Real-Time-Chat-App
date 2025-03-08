034b156793db1322b780fb5ce5853e1df7e67991
codershivam45 <145230293+codershivam45@users.noreply.github.com>
2025-03-08 21:10:19 +0530

    Section Removed

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -22,43 +22,43 @@ const details = () => {
                 </div>
             </div>
             <div className="other flex flex-col pb-2">
-                <div className="chatSettings px-4 py-2">
+                {/* <div className="chatSettings px-4 py-2">
                     <div className="head flex justify-between">
                         <div className="heading">Chat Settings  </div>
                         <div className="icon bg-[#4A4A58] flex justify-center p-2 rounded-full items-center cursor-pointer"><img src="./arrowDown.png" alt="" width="12" /></div>
                     </div>
                     <div className="content"></div>
-                </div>
-                <div className="Privacy px-4 py-2">
+                </div> */}
+                {/* <div className="Privacy px-4 py-2">
                     <div className="head flex justify-between">
                         <div className="heading">Privacy</div>
                         <div className="icon bg-[#4A4A58] flex justify-center p-2 rounded-full items-center cursor-pointer"><img src="./arrowDown.png" alt="" width="12" /></div>
                     </div>
                     <div className="content"></div>
-                </div>
-                <div className="SharedPhotos px-4 py-2">
+                </div> */}
+                {/* <div className="SharedPhotos px-4 py-2">
                     <div className="head flex justify-between">
                         <div className="heading">Shared Photos</div>
                         <div className="icon bg-[#4A4A58] flex justify-center p-2 rounded-full items-center cursor-pointer"><img src="./arrowDown.png" alt="" width="12" /></div>
                     </div>
                     <div className="content py-3 flex flex-col gap-2">
 
-                        {/* <div className="photo flex justify-between items-center">
+                        <div className="photo flex justify-between items-center">
               <div className="img flex items-center gap-2  ">
                 <div><img src="https://th.bing.com/th/id/OIP.GvntOdvz80txbfbW4rz2kAHaEo?w=1920&h=1200&rs=1&pid=ImgDetMain" alt="" width="32" /></div>
                 <div className="name text-sm text-gray-300">photo_10.12.2024</div>
               </div>
               <div className="icon"><img src="./download.png" alt="" width="16" /></div>
-            </div> */}
+            </div>
                     </div>
-                </div>
-                <div className="SharedFiles px-4 py-2">
+                </div> */}
+                {/* <div className="SharedFiles px-4 py-2">
                     <div className="head flex justify-between">
                         <div className="heading">Shared Files</div>
                         <div className="icon bg-[#4A4A58] flex justify-center p-2 rounded-full items-center cursor-pointer"><img src="./arrowDown.png" alt="" width="12" /></div>
                     </div>
                     <div className="content"></div>
-                </div>
+                </div> */}
                 <div className="blockUser px-4 py-2 flex justify-center">
                     <button className='bg-red-400 p-2 rounded-lg hover:bg-red-600 w-full ' onClick={() => { toggleBlock(receiverUser, currentUser.id, receiverUser.id) }}>{receiverUser.isReceiverBlocked ? "Unblock User" : "Block User"}</button>
                 </div>
